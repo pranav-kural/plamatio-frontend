44f7435b886c312653eef2ff9e009c2bac0d862a
pranav-kural <pranavkural042@gmail.com>
2024-08-29 19:49:46 -0400

    Define API endpoints for Redux RTK Query

--- app/types/backend-types.ts
+++ app/types/backend-types.ts
@@ -22,3 +22,31 @@ export type Product = {
   imageUrl: string;
   offered: boolean;
 };
+
+export type CartItem = {
+  id: number;
+  product: Product;
+  quantity: number;
+};
+
+export type Cart = {
+  id: number;
+  items: CartItem[];
+  total: number;
+};
+
+export type NewCartItem = {
+  productId: number;
+  quantity: number;
+};
+
+export type NewCart = {
+  items: NewCartItem[];
+};
+
+export type Order = {
+  id: number;
+  items: CartItem[];
+  date: string;
+  total: number;
+};
