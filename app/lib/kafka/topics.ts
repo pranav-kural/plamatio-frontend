423903e01fe2e8c1d80f3460f8d4733ead2e5111
pranav-kural <pranavkural042@gmail.com>
2024-09-14 20:17:01 -0400

    Data schema definitions for primary kafka topics

---
+++ app/lib/kafka/topics.ts
@@ -0,0 +1,78 @@
+/**
+ * List of Kafka topics
+ */
+export const topics = {
+  USER_EVENTS: 'user-events',
+  CART_UPDATES: 'cart-updates',
+  ORDERS_UPDATES: 'orders-updates',
+  USERS_UPDATES: 'users-updates',
+};
+
+/**
+ * Schema for UserEventsMessage
+ */
+export type UserEventsMessage = {
+  ref_id: string;
+  client_id: string;
+  event_type: string;
+  core_component: string;
+  description: string;
+  metadata: string;
+  timestamp: string;
+};
+
+/**
+ * Schema for CartUpdatesMessage
+ */
+export type CartUpdatesMessage = {
+  event_type: string;
+  cart_item: {
+    id: number;
+    product_id: number;
+    quantity: number;
+    user_id: number;
+  };
+};
+
+/**
+ * Schema for OrdersUpdatesMessage
+ */
+export type OrdersUpdatesMessage = {
+  event_type: string;
+  order: {
+    id: number;
+    user_id: number;
+    address_id: number;
+    total_price: number;
+    created_at: string;
+    status: string;
+  };
+  order_items: {
+    id: number;
+    order_id: number;
+    product_id: number;
+    quantity: number;
+  }[];
+};
+
+/**
+ * Schema for UsersUpdatesMessage
+ */
+export type UsersUpdatesMessage = {
+  event_type: string;
+  user: {
+    id: number;
+    first_name: string;
+    last_name: string;
+    ref_id: string;
+  };
+  addresses: {
+    id: number;
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+    zip_code: string;
+    user_id: number;
+  }[];
+};
